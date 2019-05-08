ba5801a7e4b3bd9eab534cc0344a8298ff947e0e
Kim Zhang <ZHAN712@sh.arvatochina.com>
2019-05-08 14:07:31 +0800

    feat change db address

--- app/db/index.js
+++ app/db/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
     user: 'root',
-    password: '0715.shaK',
-    host: '127.0.0.1',
+    password: '123456',
+    host: '120.55.55.34',
     port: '3306',
     database: 'test',
-    timezone:"08:00"
+    timezone: "08:00"
 })
 db.connect()
 module.exports = db
